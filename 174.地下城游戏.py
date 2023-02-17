b9a2e80c79cd5a59eff0eba9e69c60eeb34cc779
xxl10000 <3170101301@zju.edu.cn>
2023-02-17 20:00:13 +0800

    add format document

--- 174.地下城游戏.py
+++ 174.地下城游戏.py
@@ -36,60 +36,64 @@ from typing import List
 #                 else:
 #                     if x1 > x2:
 #                         dp[i][j][1] = y1
-#                         dp[i][j][0] = x1 
+#                         dp[i][j][0] = x1
 #                     else:
 #                         dp[i][j][1] = y2
-#                         dp[i][j][0] = x2  
-#         print(dp)                     
+#                         dp[i][j][0] = x2
+#         print(dp)
 #         return max(1 - dp[m - 1][n - 1][1],1)
 
-#低 -> 上 ， dp[i][j]: (i,j)到（m-1,n-1)对应最低血量。类似迷宫从出口开始反推          
-class Solution:  
+# 低 -> 上 ， dp[i][j]: (i,j)到（m-1,n-1)对应最低血量。类似迷宫从出口开始反推
+
+
+class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         m, n = len(dungeon), len(dungeon[0])
         dp = [[0] * n for _ in range(m)]
-        dp[m - 1][n - 1] = 1 -(dungeon[m - 1][n - 1]) if (dungeon[m - 1][n - 1]) < 0 else 1
-        
-        #可以使用虚拟头结点，虚拟边界类似方法统一，外边界填充题目不可取得的大值。
-        #dp = [[1e5] * (n + 1) for _ in range(m + 1)]
+        dp[m - 1][n - 1] = 1 - (dungeon[m - 1][n - 1]
+                                ) if (dungeon[m - 1][n - 1]) < 0 else 1
+
+        # 可以使用虚拟头结点，虚拟边界类似方法统一，外边界填充题目不可取得的大值。
+        # dp = [[1e5] * (n + 1) for _ in range(m + 1)]
         for i in range(m - 2, -1, -1):
             dp[i][n - 1] = dp[i + 1][n - 1] - dungeon[i][n - 1]
             if dp[i][n - 1] <= 0:
                 dp[i][n - 1] = 1
-            
-        for j in range(n - 2, -1 , -1):
+
+        for j in range(n - 2, -1, -1):
             dp[m - 1][j] = dp[m - 1][j + 1] - dungeon[m - 1][j]
             if dp[m - 1][j] <= 0:
                 dp[m - 1][j] = 1
         for i in range(m - 2, - 1, -1):
             for j in range(n - 2, -1, -1):
                 dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]
-                if dp[i][j ] <= 0:
+                if dp[i][j] <= 0:
                     dp[i][j] = 1
-        #print(dp)
+        # print(dp)
         return dp[0][0]
 
-# 顶 -> 下, dp(i, j): (i,j)到（m-1,n-1)对应最低血量。 
+# 顶 -> 下, dp(i, j): (i,j)到（m-1,n-1)对应最低血量。
+
+
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         @cache
         def dp(i, j):
             if i == len(dungeon) - 1 and j == len(dungeon[0]) - 1:
                 return 1 - dungeon[i][j] if dungeon[i][j] < 0 else 1
-            
+
             if i == len(dungeon) or j == len(dungeon[0]):
                 return 1e9
-            
+
             res = min(dp(i + 1, j), dp(i, j + 1)) - dungeon[i][j]
             if res <= 0:
                 res = 1
             return res
-        
-        return dp(0, 0)  
-             
-#Debug       
+
+        return dp(0, 0)
+
+# Debug
 # obj = Solution()
 # print(obj.calculateMinimumHP([[1,-3,3],[0,-2,0],[-3,-3,-3]]))
 
 # @lc code=end
-

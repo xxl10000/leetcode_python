598138486d98aaad42feeccc14e1d3d1800a2266
xxl10000 <3170101301@zju.edu.cn>
2023-03-06 16:56:46 +0800

    add bfs

--- graph/797.所有可能的路径.py
+++ graph/797.所有可能的路径.py
@@ -26,26 +26,51 @@
 #         return res
 
 # dfs, 处理结点 注意结束条件，如果直接return，还要前面加上pop()
+# class Solution:
+#     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+#         res, onpath = [], []
+        
+#         def dfs(start):
+#             nonlocal res, onpath
+            
+#             onpath.append(start)
+            
+#             if start == len(graph) - 1:
+#                 res.append(onpath[:])
+                
+            
+#             for neighbor in graph[start]:
+#                 dfs(neighbor)
+            
+#             onpath.pop()
+        
+#         dfs(0) 
+#         return res    
+
+# bfs 主要怎么进队列和进队列的参数 
+from collections import deque
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         res, onpath = [], []
+        queue = deque()
+        onpath.append(0)
+        queue.append((0, onpath))
         
-        def dfs(start):
-            nonlocal res, onpath
-            
-            onpath.append(start)
-            
-            if start == len(graph) - 1:
-                res.append(onpath[:])
-                
-            
-            for neighbor in graph[start]:
-                dfs(neighbor)
-            
-            onpath.pop()
-        
-        dfs(0) 
-        return res     
+        while queue:
+            sz = len(queue)
+            for i in range(sz):
+                node, path = queue.popleft()
+                if node == len(graph) - 1:
+                    res.append(path[:])
+                for neighbor in graph[node]:
+                    # 法1
+                    #queue.append((neighbor, path + [neighbor]))
+                    
+                    # 法2， 传入参数为path[:]，不是path，原因同复制一个列表，不然会改变
+                    path.append(neighbor)
+                    queue.append((neighbor,path[:]))
+                    path.pop()
+        return res
         
 # @lc code=end
 
